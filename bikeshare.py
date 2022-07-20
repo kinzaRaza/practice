4d2eb303c7783eef96371243817b0fae1f6e332a
Student <lab-103@aptech-msg.edu.pk>
2022-07-20 17:57:54 +0500

    first commit
    add bikeshare.py and .gitignore

--- bikeshare.py
+++ bikeshare.py
@@ -22,7 +22,7 @@ def get_filters():
     # TO DO: get user input for month (all, january, february, ... , june)
 
 
-    # TO DO: get user input for day of week (all, monday, tuesday, ... sunday)
+    # TO DO: get user input for day of week (all, monday, tuesday, .... sunday)
 
 
     print('-'*40)
